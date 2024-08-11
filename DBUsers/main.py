12f8a4478523e9394e71caefb91f9b1aaf8a92e9
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-11 09:26:14 -0230

    Added change_role and delete_user to main.py

--- DBUsers/main.py
+++ DBUsers/main.py
@@ -12,4 +12,26 @@ def add_user():
         role = role
     )
 
-add_user()
+def change_role():
+    user_id = int(input("Enter user ID: "))
+    role = input("Enter a new role: ")
+
+    user = User.objects.get(id=user_id)
+    user.role = role
+    user.save()
+
+def delete_user():
+    user_id = int(input("Enter user ID: "))
+
+    User.objects.get(id = user_id).delete()
+
+while True:
+    choice = int(input("Select an option:\n1.Add user\n2.Change role\n3.Delete user\n4.Exit\n"))
+    if choice == 1:
+        add_user()
+    elif choice == 2:
+        change_role()
+    elif choice == 3:
+        delete_user()
+    elif choice == 4:
+        break
