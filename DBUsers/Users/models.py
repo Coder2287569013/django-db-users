bac7d631d85dca8e6070367939a3ace607d4e83f
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-11 10:03:15 -0230

    Add a new model, delete main.py and d_setup.py

--- DBUsers/Users/models.py
+++ DBUsers/Users/models.py
@@ -11,4 +11,19 @@ class User(models.Model):
     role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
 
     def __str__(self) -> str:
-        return self.name, self.role
+        return f"{self.name}, {self.role}"
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        DONE = "DONE", "done"
+        PROCESSING = "PROCESSING", "processing"
+        NO_WORK = "NO WORK", "no work"
+
+    status = models.CharField(max_length=100, choices=Status.choices, default=Status.NO_WORK)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey("User", on_delete=models.CASCADE, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}, {self.status}"
